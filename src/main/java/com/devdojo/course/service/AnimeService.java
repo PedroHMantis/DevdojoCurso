b8699542dfc7bef6f102e0fe52d02dd0a61950d5
PedroHMantis <gtx1050pd@gmail.com>
2026-01-20 22:15:37 -0300

    DevDojo_Aula08

--- src/main/java/com/devdojo/course/service/AnimeService.java
+++ src/main/java/com/devdojo/course/service/AnimeService.java
@@ -1,15 +1,25 @@
 package com.devdojo.course.service;
 
 import com.devdojo.course.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
 
+    private List<Anime> animes = List.of(new Anime(1L,"Naruto"), new Anime(2L,"One Piece"), new Anime(3L,"Bleach"), new Anime(3L,"DBZ"));
+
     public List<Anime> listAll(){
-        return List.of(new Anime(1L,"Naruto"), new Anime(2L,"One Piece"), new Anime(3L,"Bleach"),
-                new Anime(4L,"Shoujo Ramune"), new Anime(5L,"Naruto Shippuden gay"), new Anime(3L,"DBZ"));
+        return animes;
+    }
+
+    public Anime findById(Long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
     }
 }
